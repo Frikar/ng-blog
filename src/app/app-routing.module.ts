9310ab6f4cb7c25ae70b51616247b4f4132c145c
Diego <ddglobaltravel1@gmail.com>
2021-02-04 00:07:00 -0400

    Lazy loading routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {AboutComponent} from './about/about.component';
+import {CommonModule} from '@angular/common';
+import {QuicklinkModule, QuicklinkStrategy} from 'ngx-quicklink';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
-  {path: '**', component: HomeComponent},
+  {path: 'post/:id/:slug', loadChildren: () => import('./post/post.module').then(m => m.PostModule)},
+  {path: '**', component: HomeComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    QuicklinkModule,
+    RouterModule.forRoot(routes, {preloadingStrategy: QuicklinkStrategy})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
